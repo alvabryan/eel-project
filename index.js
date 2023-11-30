03e7320215f8d6ba616d48cbd421bd91502826dd
Bryan Alvarenga <alvarengabryan@hotmail.com>
2023-11-29 20:37:06 -0500

    Added image classification and sentment analysis for text data

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const functions = require("firebase-functions");
 const multer = require("multer");
 const { startMetricsServer, restResponseTimeHistogram } = require("./utils/metrics.js");
 const responseTime = require("response-time");
+var Sentiment = require('sentiment');
+
+// Image classification
+const tf = require('@tensorflow/tfjs');
+const mobilenet = require('@tensorflow-models/mobilenet');
+const tfnode = require('@tensorflow/tfjs-node');
+const fs = require('fs');
 
 const app = express();
 
@@ -47,31 +54,46 @@ app.use(
 // Missing:
 // Data type: text, Bulk sensor data (Jeffrey)
 // Script that is going to run on IOT DEVICE (Jeffrey, Avinash)
-    // Traverse through all the images and make a request per image
-    // Traverse through text and make request per text
-    // Traverse bulk data and make request per line assuming data is structured in CSV (Make request per object key:value)
+// Traverse through all the images and make a request per image
+// Traverse through text and make request per text
+// Traverse bulk data and make request per line assuming data is structured in CSV (Make request per object key:value)
 // Server API endpoint processing: Classification Model, Processing for text-upload end-point (Bryan)
+
+const imageClassification = async image => {
+    const mobilenetModel = await mobilenet.load();
+    const predictions = await mobilenetModel.classify(image);
+    return predictions;
+}
 
 // IMPLEMENT FILE UPLOAD ROUTE
 // VIDEO AND PICTURES
 app.post("/image-upload", multParse.single('file'), (req, res) => {
-    setTimeout(() => {
-        console.log('File uploaded successfully:', req.file.originalname);
-        res.send('File uploaded successfully: ' + req.file.originalname);
-    }, Math.random()*1000);
+    if (!req.file) {
+        res.status(400).send("Please upload a valid image");
+    }
+
+    const tfimage = tfnode.node.decodeImage(req.file.buffer);
+    const predictions = imageClassification(tfimage);
+    predictions.then((pred) => {
+        let predictionText = 'Predictions:\n';
+        pred.forEach((tmp) => {
+            predictionText += `${tmp.className}: ${tmp.probability * 100}%\n`
+        });
+        res.send('File processed successfully: ' + req.file.originalname + '\n ' + predictionText);
+    });
 });
 
 // IMPLEMENT TEXT UPLOAD ROUTE
 // ACCEPT: Just text (plain text, csv line - bulk data)
 app.post("/text-upload", (req, res) => {
-    // console.log(req.body);
-    // res.send("Received");
+
     if (!req.body.data) {
         return res.status(400).send('No data was uploaded.');
     }
 
-    console.log('Data uploaded successfully:', req.body.data);
-    res.send('Data uploaded successfully: ' + req.body.data);
+    var sentiment = new Sentiment();
+    var result = sentiment.analyze(req.body.data);
+    res.send('Data processed successfully: ' + req.body.data + '\n' + 'Sentiment Score: ' + result.score);
 });
 
 // exports.app = functions.https.onRequest(app);
